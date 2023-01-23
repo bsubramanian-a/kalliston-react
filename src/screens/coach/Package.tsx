704baadee85129364c2107c7e002f77a5e10c12e
Rajavel-Balasubramanian <bsubramanian.a@gmail.com>
2023-01-23 21:12:10 +0530

    media and user details

--- src/screens/coach/Package.tsx
+++ src/screens/coach/Package.tsx
@@ -8,15 +8,19 @@ import LeftMenu from './components/LeftMenu';
 import TopNav from './components/TopNav';
 import { useSelector } from 'react-redux';
 import { useGetPackagesQuery } from '../../services/package.service';
+import { useGetCoachQuery } from '../../services/coach-service';
+import { useGetMediasQuery } from '../../services/media-service';
 
 function Package() {
     const { data: packages = [], isFetching, isError, error }:any = useGetPackagesQuery(1);
-
+    const { data: currentUser = []}:any = useGetCoachQuery(1);
+    const { data: medias = [], isLoading, refetch }:any = useGetMediasQuery(1);
     const [basic, setBasic] = useState<any>();
     const [premium, setPremium] = useState<any>();
     const [elite, setElite] = useState<any>();
+    console.log("currentUser", currentUser);
 
-    const { coach: currentUser } = useSelector((state:any) => state.auth);
+    // const { coach: currentUser } = useSelector((state:any) => state.auth);
 
     useEffect(() => {
         setBasic(packages.find((cpackage:any) => cpackage.package_type == 'basic'));
@@ -60,42 +64,47 @@ function Package() {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="position-relative mb-5 pro-cover">
-                                            <div className="position-absolute" style={{bottom: "-40%", left: "2%"}}><img className="me-3 img-profile rounded-circle profile_pic" src={currentUser?.avatar || User} /><span className="f-color fw-medium big-font">{currentUser?.firstname+" "+currentUser?.lastname}</span></div>
+                                        <div className="position-relative mb-5 pro-cover" style={{backgroundImage: `url(${currentUser?.cover_image})`}}>
+                                            <div className="position-absolute" style={{bottom: "-40%", left: "2%"}}><img className="me-3 img-profile rounded-circle profile_pic" src={currentUser?.avatar || User} /><span className="f-color fw-medium big-font">{(currentUser?.firstname || "")  +" "+(currentUser?.lastname || "")}</span></div>
                                         </div>
                                         <div className="card-body pt-4">
                                             <div className="row">
-                                                <div className="col col-12 mb-4">
-                                                    <div className="row gap-2 gap-lg-0">
-                                                        <div className="col col-12 col-lg-6">
-                                                            <h1 className="l-size f-color fw-semibold mb-0">Trainning category</h1>
-                                                        </div>
-                                                        <div className="col col-12 col-lg-6">
-                                                            <div className="d-flex justify-content-lg-end align-items-center"><span className="l-size f-color fw-regular mb-0">General training</span></div>
+                                                {currentUser?.category && 
+                                                   <div className="col col-12 mb-4">
+                                                        <div className="row gap-2 gap-lg-0">
+                                                            <div className="col col-12 col-lg-6">
+                                                                <h1 className="l-size f-color fw-semibold mb-0">{currentUser?.category}</h1>
+                                                            </div>
+                                                            <div className="col col-12 col-lg-6">
+                                                                <div className="d-flex justify-content-lg-end align-items-center"><span className="l-size f-color fw-regular mb-0">General training</span></div>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
+                                                }
+
                                                 <div className="col col-12 mb-4">
                                                     <h1 className="l-size f-color fw-semibold mb-3">Bio</h1>
-                                                    <p className="l-size fw-regular mb-0 f-color">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                                                    {currentUser?.bio ? 
+                                                        <p className="l-size fw-regular mb-0 f-color">{currentUser?.bio}</p> : <p>No bio given</p>
+                                                    }
+                          
                                                     <div className="d-flex justify-content-between align-items-center"></div>
                                                 </div>
                                                 <div className="col col-12 mb-4">
                                                     <h1 className="l-size f-color fw-semibold mb-3">Media</h1>
+                                                    {medias?.length > 0 ? 
                                                     <div className="row">
-                                                        <div className="col col-6 col-md-3 mb-3 mb-md-0">
-                                                            <div className="w-100 n-br package-div"></div>
-                                                        </div>
-                                                        <div className="col col-6 col-md-3 mb-3 mb-md-0">
-                                                            <div className="w-100 n-br package-div"></div>
-                                                        </div>
-                                                        <div className="col col-6 col-md-3">
-                                                            <div className="w-100 n-br package-div"></div>
-                                                        </div>
-                                                        <div className="col col-6 col-md-3">
-                                                            <div className="w-100 n-br package-div"></div>
-                                                        </div>
-                                                    </div>
+                                                        {medias?.map((media:any) => {
+                                                            return(
+                                                                <div className="col col-6 col-md-3 mb-3 mb-md-0">
+                                                                    <img src={media?.image} alt="" className='w-100 h-100' />
+                                                                </div>
+                                                            )
+                                                        })}
+                                                        
+                                                    </div> : <p>No media uploaded</p>
+                                                    }
+                                                    
                                                 </div>
                                                 <div className="col col-12 d-flex justify-content-end align-items-center">
                                                     <Link to={"/coach/profile"} className="px-3 text-decoration-none ll-div d-flex justify-content-center align-items-center n-br"><img className="me-2" src={Modify} /><span className="m-p fw-medium text-uppercase">Modify</span></Link>
